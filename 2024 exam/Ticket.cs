838cbdb8b75a8e3f1e597c39c7516c369fe0a6a8
Martin Durkin <S00236994@atu.ie>
2025-12-12 15:41:56 +0000

    added a ticket class

---
+++ 2024 exam/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_exam
+{
+    public class Ticket
+    {
+        //properties
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int AvailableTickets { get; set; }
+
+        //constructors
+
+        //methods
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+
+
+    }//end of class
+}
