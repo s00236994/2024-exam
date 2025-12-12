90ebefc9d4490c8df0456622ae7f51a60a9147be
Martin Durkin <S00236994@atu.ie>
2025-12-12 15:48:18 +0000

    added few more classes and icomparable

---
+++ 2024 exam/Event.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_exam
+{
+    public enum EventType
+    {
+        Music, Comedy, Theatre
+    }
+    public class Event : IComparable<Event>
+    {
+        //properties
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; }
+
+        public List <Ticket> Tickets { get; set; } 
+
+        public Event TypeOfEvent { get; set; }
+
+
+
+        //constructors
+
+        //methods
+        public int CompareTo(Event other)
+        {
+            return this.EventDate.CompareTo(other.EventDate);
+        }
+    }
+}
---
+++ 2024 exam/VIPTicket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_exam
+{
+    public class VIPTicket : Ticket
+    {
+        //properties
+        public string AdditionalExtras { get; set; }
+
+        public decimal AdditionalCost { get; set; }
+
+        //constructors
+
+
+        //methods
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
